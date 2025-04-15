581ea62f5bfd971b87bd38808b8a3164458149e8
miriyan254 <miriyan254@gmail.com>
2025-04-15 23:40:51 +0200

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
-function Button({children}){
-	return <button type='button' className={`${children === 'New Game' ? 'new' : ''}`} >{children}</button>;
+function Button({children, handleOnClick}){
+	return <button type='button' className={`${children === 'New Game' ? 'new' : 'square'}`} onClick = {handleOnClick}>{children}</button>;
 }
 
 export default Button;
