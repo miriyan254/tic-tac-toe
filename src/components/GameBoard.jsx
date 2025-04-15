cb2e9c81c57b8c61f3f37cdbf2b04303bcd3e7ae
miriyan254 <miriyan254@gmail.com>
2025-04-15 23:43:07 +0200

    Update GameBoard.jsx

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import Button from './Button';
 
 const initialBoard = [null, null, null, null, null, null, null, null, null];
 
+function deriveGameBoard(gms) {
+  const board = initialBoard;
+  for (const gm of gms) { board[gm.sqInd] = gm.sqMark; }
+  return board;
+}
+
 function GameBoard() {
+  const [moves, setMoves] = useState([]);
+
+  function handleMove(ind) {  
+    setMoves( prev => [{ sqInd: ind, sqMark: prev[0]?.sqMark === 'X' ? 'O' : 'X'}, ...prev] );
+  }
+
+  const currBoard = deriveGameBoard(moves);
 
   return (
       	<ul>
-		{ initialBoard.map( (mark, i) => <li key={i}><Button>{mark}</Button></li> ) }
+		{currBoard.map( (mark, i) => <li key={i}><Button handleOnClick={() => handleMove(i)}>{mark}</Button></li> )}
 	</ul>   
   );
   
