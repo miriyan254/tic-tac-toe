3c84bcf141a75a88f8ecce2c48d8d4afa1727912
miriyan254 <miriyan254@gmail.com>
2025-04-17 01:14:02 +0300

    Update GameBoard.jsx

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -9,6 +9,16 @@ function deriveGameBoard(gms) {
   return board;
 }
 
+function deriveWinner(gB) {
+  const wins = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ];
+  let winner;
+
+  for (const win of wins) {
+    if (gB[win[0]] && gB[win[0]] === gB[win[1]] && gB[win[0]] === gB[win[2]] ) { winner = win; }
+  }
+  return winner;
+}
+
 function GameBoard() {
   const [moves, setMoves] = useState([]);
 
@@ -17,6 +27,7 @@ function GameBoard() {
   }
 
   const currBoard = deriveGameBoard(moves);
+  const winComb = deriveWinner(currBoard);
 
 function handleNewGame(){
   setMoves([]);
@@ -25,7 +36,7 @@ function handleNewGame(){
   return (
     <main>
       <ul>
-        {currBoard.map( (mark, i) => <li key={i}><Button handleOnClick={() => handleMove(i)}>{mark}</Button></li> )} 
+        {currBoard.map( (mark, i) => <li key={i}><Button handleOnClick={() => handleMove(i)} i={i} win={winComb}>{mark}</Button></li> )} 
       </ul>
       <Button handleOnClick={handleNewGame}>New Game</Button>  
     </main>   
